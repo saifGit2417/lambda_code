746280926f11933e4c87c0885e16d07797325a8b
Md Saif Ur Rahman <saifwork2417@gmail.com>
2024-06-30 02:00:42 +0530

    P-11 v-01 done

---
+++ 11/01.js
@@ -0,0 +1,25 @@
+// function call(task) {
+//   setTimeout(() => {
+//     console.log("task is", task);
+//   }, 3000);
+// }
+
+// console.log("start");
+// call(`saif`);
+// console.log("end");
+
+function counter() {
+  let i = 0;
+  function increaseMe() {
+    i += 1;
+    return i;
+  }
+  return increaseMe;
+}
+
+let count = counter();
+
+console.log(`first`, count());
+console.log(`first`, count());
+console.log(`first`, count());
+console.log(`first`, count()-9);
